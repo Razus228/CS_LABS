fae6b53bce8fe7769e197df2e5057dd1d316e4fe
Razus228 <cassilasq2@gmail.com>
2023-09-28 09:04:35 +0300

    optimizing

--- Lab1/caesar.py
+++ Lab1/caesar.py
@@ -33,25 +33,23 @@ def remove_duplicates(input_string):
     return output_string
 
 
-# Apply the shift to the obtained alphabet
 def rotate_left(input_string, n):
     n = n % len(input_string)
     return input_string[n:] + input_string[:n]
 
 def menu(input_number):
-    while True:
-        if input_number == 1:
-            print(caesar_cipher(enc_mess, 3, operations[0], alphabet))
-            exit()
-        elif input_number == 2:
-            print(caesar_cipher(dec_mess, 3, operations[1], alphabet))
-            exit()
-        elif input_number == 3:
-            print(caesar_cipher_extended(enc_mess, 3, key, operations[0], alphabet))
-            exit()
-        else:
-            print(caesar_cipher_extended(dec_mess, 1, key, operations[1], alphabet))
-            exit()
+    if input_number == 1:
+        print(caesar_cipher(enc_mess, 3, operations[0], alphabet))
+        exit()
+    elif input_number == 2:
+        print(caesar_cipher(dec_mess, 3, operations[1], alphabet))
+        exit()
+    elif input_number == 3:
+        print(caesar_cipher_extended(enc_mess, 3, key, operations[0], alphabet))
+        exit()
+    else:
+        print(caesar_cipher_extended(dec_mess, 1, key, operations[1], alphabet))
+        exit()
 
 # Test cases
 if __name__ == "__main__":
@@ -60,11 +58,6 @@ if __name__ == "__main__":
     enc_mess = input("Message to be encrypted : ")
     dec_mess = input("Message to be decrypted : ")
     key = input("Input secret key : ")
-    i = input("What do you want to do : ")
+    i = input("What fo you wnat to do? ")
     i = int(i)
     print(menu(i))
-
-    #print(caesar_cipher(enc_mess, 3, operations[0], alphabet))
-    #print(caesar_cipher(enc_mess, 3, operations[1], alphabet))
-    #print(caesar_cipher_extended(enc_mess, 3, key, operations[0], alphabet))
-    #print(caesar_cipher_extended(enc_mess, 3, key, operations[1], alphabet))
